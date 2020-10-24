5afd3e34be8ef0ab3f36cf642a0678e4cb1151ad
Ramya.U <ramyayadav2016@gmail.com>
2020-10-24 13:41:44 +0530

    Added UC-6

--- DAY.4-EmployeeWageComputation/Program.cs
+++ DAY.4-EmployeeWageComputation/Program.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿/******************************************************************************
+ *  Compilation:  csc.exe /t:exe /out:Day04EmployeeWageCompuatation.exe Day04EmployeeWageCompuatation.cs
+ *  Execution:    Day04EmployeeWageCompuatation.exe 103
+ *  
+ *  Purpose: Conditional Wage
+ *
+ *  @author  Ramya U
+ *  @version 1.0
+ *  @since   22-10-2020
+ *
+ ******************************************************************************/
+using System;
 
-namespace DAY._4_EmployeeWageComputation
+namespace Day04EmployeeWageComputation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to employee wage compuataion!");
+            Console.WriteLine("Welcome to EmployeeWageComputation!");
+            //constants
+            const int IS_FULL_TIME = 2;
+            const int EMP_RATE_PER_HOUR = 20;
+            const int IS_PART_TIME = 1;
+            const int NUM_OF_WORKING_DAYS = 2;
+            const int MAX_HRS_IN_MONTH = 10;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            //Computation
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                        totalEmpHrs += empHrs;
+                        Console.WriteLine("Days:" + totalWorkingDays + "Emp Hrs : " + empHrs);
+                }
+                int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Total Employee Wage is : " + totalEmpWage);
+            }
         }
     }
 }
