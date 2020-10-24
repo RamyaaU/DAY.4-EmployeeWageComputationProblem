d0dc4bcc1cb8c5a4a66c726d5e0d039a2828ca14
Ramya.U <ramyayadav2016@gmail.com>
2020-10-24 15:51:10 +0530

    Added UC-10

---
+++ DAY.4-EmployeeWageComputation/EmpWageBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY._4_EmployeeWageComputation
+{
+    public class EmpWageBuilder
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
